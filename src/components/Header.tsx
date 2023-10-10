fb99aa05107106560595e04a7da85f76bf17f3f2
HugoWebDev <mrhuman2709@gmail.com>
2023-10-10 10:50:43 -0700

    header finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "../styles/header.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
     return (
-      <div>Header</div>
-    )
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">Britlex</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#features">Skills</Nav.Link>
+              <Nav.Link href="#features">About Us</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="#pricing">Contacts</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">
+                <button className="btn btn-outline-dark px-4 py-2">Let's Talk</button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
   }
 }
+
+export default Header;
