4d9511ac0c099423e1f493176d0189e6e1a74f87
Uzgur <uzbmv2@gmail.com>
2023-10-10 23:15:55 +0500

    react-bootstrap qo'shildi

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import Skills from "./components/Skills";
+// import Skills from "./components/Skills";
 
 import "./styles/style.css";
 
