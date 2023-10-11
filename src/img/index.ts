cabd7b733b77a83802d00b8ce756ab181a09eb2a
Uzgur <uzbmv2@gmail.com>
2023-10-11 17:41:07 +0500

    Hero finished

---
+++ src/img/index.ts
@@ -0,0 +1,6 @@
+import AboutUsImg from "./AboutUsImg";
+import HeroImg from "./HeroImg";
+import LampImg from "./LampImg";
+import LogoImg from "./LogoImg";
+
+export { AboutUsImg, HeroImg, LampImg, LogoImg };
