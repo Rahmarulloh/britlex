d6ebdd2251daad9a01ac15722259f1daacec0990
Uzgur <uzbmv2@gmail.com>
2023-10-11 21:56:40 +0500

    abduvali merged to master

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -3,6 +3,6 @@ import "../styles/contactUs.css";
 
 export default class ContactUs extends Component {
   render() {
-    return <div>Contact Us</div>;
+    return <div className="contactUs">Contact Us</div>;
   }
 }
