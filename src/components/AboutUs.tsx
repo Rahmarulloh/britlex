3c29a68b8212dc870a0a966ae1a841de3aa5538d
Abduvali <abduvalikhaydarov005@gmail.com@example.com>
2023-10-11 18:16:23 +0500

    all responsives done

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,33 +1,36 @@
 import React, { Component } from "react";
 import "../styles/aboutUs.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { default as AboutUsImg } from "../img/AboutUsImg";
-
+import AboutUsImg from "../img/aboutUsImg.png";
 class AboutUs extends Component {
     render(): React.ReactNode {
         return (
             <section className="aboutUs">
-                <div className="aboutUs-left">
+                <div className="aboutUs-headline">
                     <h2>About Us</h2>
-                    <p>The model offers a framework for discussing problems related to the user's experience, as well as possible ways and means of solving them. Application development begins at the top level (strategy), where the future software product is described quite abstractly from the point of view of the expectations of both users and the customer.
-                    </p>
-                    <div className="aboutUs-nums">
-                        <span>
-                            <strong>800</strong>
-                            <p>Pupils</p>
-                        </span>
-                        <span>
-                            <strong>18</strong>
-                            <p>Teachers</p>
-                        </span>
-                        <span>
-                            <strong>6</strong>
-                            <p>Foreign language</p>
-                        </span>
-                    </div>
                 </div>
-                <div className="aboutUs-right">
-                    <AboutUsImg/>
+                <div className="aboutUs-main">
+                    <div className="aboutUs-left">
+                        <p>The model offers a framework for discussing problems related to the user's experience, as well as possible ways and means of solving them. Application development begins at the top level (strategy), where the future software product is described quite abstractly from the point of view of the expectations of both users and the customer.
+                        </p>
+                        <div className="aboutUs-nums">
+                            <span>
+                                <strong>800</strong>
+                                <p>Pupils</p>
+                            </span>
+                            <span>
+                                <strong>18</strong>
+                                <p>Teachers</p>
+                            </span>
+                            <span>
+                                <strong>6</strong>
+                                <p>Foreign language</p>
+                            </span>
+                        </div>
+                    </div>
+                    <div className="aboutUs-right">
+                        <img src={AboutUsImg} alt="img" />
+                    </div>
                 </div>
             </section>
         )
