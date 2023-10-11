b0b21bc4550a3b13a3cc05ce541df9e206db9b46
Uzgur <uzbmv2@gmail.com>
2023-10-11 19:34:57 +0500

    hero mobile continuing

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Header />
       <Skills />
-      <AboutUs />
+      {/* <AboutUs /> */}
       {/* <Pricing /> */}
       <ContactUs />
     </div>
