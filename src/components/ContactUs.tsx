29334f1a7b850ebe71c3065022e66ed4025126d0
Uzgur <uzbmv2@gmail.com>
2023-10-11 23:06:54 +0500

    ContactUs section started

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 import "../styles/contactUs.css";
+import { ContactUsImg } from "../img";
 
 export default class ContactUs extends Component {
   render() {
-    return <div className="contactUs">Contact Us</div>;
+    return (
+      <div className="contactUs">
+        <div className="contactUs-image">
+          <ContactUsImg />
+        </div>
+
+        <div className="contactUs-context">
+          <h2 className="contactUs-title">Contact Us</h2>
+
+          <p className="contactUs-desc">
+            Discover your current English level by taking our free online
+            English test.Sign up to our newsletter for learning tips and free
+            resources
+          </p>
+
+          <div className="contactUs-form">
+            <input
+              type="email"
+              className="form-email"
+              placeholder="Enter your E-mail"
+            />
+
+            <button className="form-btn">Subscribe</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
--- src/img/index.ts
+++ src/img/index.ts
@@ -2,5 +2,6 @@ import AboutUsImg from "./aboutUsImg.png";
 import HeroImg from "./HeroImg";
 import LampImg from "./LampImg";
 import LogoImg from "./LogoImg";
+import ContactUsImg from "./ContactUsImg";
 
-export { AboutUsImg, HeroImg, LampImg, LogoImg };
+export { AboutUsImg, HeroImg, LampImg, LogoImg, ContactUsImg };
